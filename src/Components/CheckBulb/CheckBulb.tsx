491677dddeae1f4a9a6901d522e6066a756c7468
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-21 20:17:27 +0600

    Check Bulb Page SImpley Added

--- src/Components/CheckBulb/CheckBulb.tsx
+++ src/Components/CheckBulb/CheckBulb.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import bulbStyle from "../../assets/Styles/CheckBulb.module.css";
+import bulbOff from "../../assets/Images/Bulb-Off.png";
+import bulbOn from "../../assets/Images/Bulb-On.png";
 
 const CheckBulb = () => {
+    const [bulb, setBulb] = useState(bulbOff)
     return (
-        <div>
-            <h1>Bulb Switch</h1>
+        <div className={bulbStyle.container}>
+            <h1>Click A Button to On/Off the Bulb</h1>
+            <img src={bulb} alt="Bulb" />
+            <div>
+                <button onClick={() => setBulb(bulbOn)} className={bulbStyle.onButton}>ON</button>
+                <button onClick={() => setBulb(bulbOff)} className={bulbStyle.offButton}>OFF</button>
+            </div>
         </div>
     );
 };
