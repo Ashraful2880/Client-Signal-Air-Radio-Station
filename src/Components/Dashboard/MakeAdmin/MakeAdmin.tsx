4243ce579e059432265b4cdcd78e2bded9e7ba67
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-24 15:47:03 +0600

    Make Admin Page Design Added

--- src/Components/Dashboard/MakeAdmin/MakeAdmin.tsx
+++ src/Components/Dashboard/MakeAdmin/MakeAdmin.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import adminStyle from "../../../assets/Styles/MakeAdmin.module.css";
 
 const MakeAdmin = () => {
     return (
-        <div>
-            Make An Admin
+        <div className={adminStyle.adminbg}>
+            <div className={adminStyle.adminContainer}>
+                <div>
+                    <input className={adminStyle.email} type="text" placeholder="Enter New Admin Email" />
+                </div>
+                <div>
+                    <input className={adminStyle.submit} type="submit" value="Confirm" />
+                </div>
+            </div>
         </div>
     );
 };
