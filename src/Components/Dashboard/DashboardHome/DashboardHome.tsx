c01cc2338f460887f6119e13299ec4c7c237c7c6
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-24 15:28:06 +0600

    Dashboard Home Design Added

--- src/Components/Dashboard/DashboardHome/DashboardHome.tsx
+++ src/Components/Dashboard/DashboardHome/DashboardHome.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import style from "../../../assets/Styles/DashboardHome.module.css";
 
 const DashboardHome = () => {
     return (
-        <div>
-            Welcome to Dashboard Home Page
+        <div className={style.bgStyle}>
+            <div className={style.container}>
+                <img src="https://lh3.googleusercontent.com/a-/AOh14GjtMugKN01IsyUPYhjf8nysWcxVPMO8pSm0E-CN1A=s96-c" alt="User" />
+                <h1>Hi,</h1>
+                <h3>User Name</h3>
+                <h2>Welcome to <span>Signal Air Radio Station</span> </h2>
+                <p>Do you want to control your expenses and be always aware of how much money you have spent?</p>
+            </div>
         </div>
     );
 };
